860fff543ceaba71cf64fb67bf3eaffd0bb554ba
Diego Peralta M <diego.pmz08@gmail.com>
2025-03-17 19:44:36 -0600

    Eliminados dependencias innecesarias y configuraciones relacionadas con Vercel en package.json y astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,9 @@
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel";
-
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
     defaultStrategy: "viewport",
     prefetchAll: false,
   },
-
-  image: {
-    service: {
-      entrypoint: "astro/assets/services/sharp",
-    },
-  },
-
-  adapter: vercel({
-    imageService: false,
-  }),
 });
