6f49dcd8d4eb4851f114307e05a4dbf94b8924fe
Diego Peralta M <diego.pmz08@gmail.com>
2025-03-17 21:33:00 -0600

    ajustadas rutas de importación en varios archivos .astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel";
-
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
     defaultStrategy: "viewport",
     prefetchAll: false,
   },
-
-  adapter: vercel(),
 });
