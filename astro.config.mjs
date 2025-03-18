4f2ee0f3c1422060fbaf76df1779cc9eae8fbd3c
Diego Peralta M <diego.pmz08@gmail.com>
2025-03-17 21:16:55 -0600

    Añadida la dependencia '@astrojs/vercel' y configurado el adaptador en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
     defaultStrategy: "viewport",
     prefetchAll: false,
   },
+
+  adapter: vercel(),
 });
